b1639b961c2b2bac4f16a0e0de6ca9d13bb24177
Icyblue <guan8175@gmail.com>
2022-11-07 17:05:33 +0300

    starting over on script

--- script.js
+++ script.js
@@ -4,31 +4,6 @@ const operatorButtons = document.querySelectorAll('.operator-button');
 const currentDisplay = document.getElementById('current-display');
 const previousDisplay = document.getElementById('previous-display');
 
-let firstNumber = 0;
-let secondNumber = 0;
-let operator = '';
-
-function displayNum(current, previous) {
-    currentDisplay.innerHTML = current;
-    previousDisplay.innerHTML = previous;
-}
-
-operatorButtons.forEach(button => {
-    button.addEventListener('click', (e) => {
-        switch (e.target.innerHTML) {
-            case ('+'): 
-                operator = '+';
-                break;
-            case ('-'): 
-                operator = '-';
-                break;
-            case ('x'): 
-                operator = 'x';
-                break;
-            case ('/'): 
-                operator = '/';
-                break;
-        }        
-        displayNum(firstNumber + ' ' +operator)
-    })
-})
+let currentNumber = '';
+let previousNumber = '';
+let operator = undefined;
